d57c71df8633a6943819e84e556f8be73c14c508
dcgulik <38480967+dcgulik@users.noreply.github.com>
2023-05-15 23:28:57 +0000

    Added 10 nouns, each with three actions

--- main.cpp
+++ main.cpp
@@ -17,10 +17,10 @@ I will request a change be made to one of your 10 nouns for the sole purpose of 
  
  2) for each of the 10 nouns, write out 3 actions it might perform, in plain english.
     a) AVOID state-checking actions, like "Car has a horn".  i.e.
-        car.hasHorn()
+        Car.hasHorn()
     b) We use 'can' and 'has' to check the state of objects, so they aren't really 'action' verbs.
-        "the Car blows its horn" is a better action, because the car is doing something.  its state is not being checked.  i.e.
-            car.blowsHorn()
+        "the Car blows its horn" is a better action, because the Car is doing something.  its state is not being checked.  i.e.
+            Car.blowsHorn()
     c) choose actions that your noun does.  Do not write out actions that are performed on your noun.
         for example: 
             noun: lightBulb.
@@ -52,97 +52,97 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Car
+//  action 1: the Car accelerates
+Car.accelerate();
+//  action 2: the Car turns on the check engine light
+Car.checkEngineLightOn();
+//  action 3: the Car detects low windshield wiper fluid
+Car.lowWindshieldWiperFluid();
 //  2)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Person
+//  action 1: the Person walks
+Person.walk();
+//  action 2: the Person jumps
+Person.jump();
+//  action 3: the Person speaks
+Person.speak();
 //  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
-//  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Stereo
+//  action 1: the Stereo selects an input
+Stereo.selectInput();
+//  action 2: the Stereo changes the output volume
+Stereo.changeOutputVolume();
+//  action 3: the Stereo engages the subwoofer
+Stereo.engageSubwoofer();
+//  4) 
+//  Noun: Oven
+//  action 1: the Oven preheats
+Oven.preheat();
+//  action 2: the Oven notifies when preheated
+Oven.preheatNotify();
+//  action 3: the Oven notifies when a timer ends
+Oven.timerNotify();
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: N64
+//  action 1: the N64 powers on
+N64.powerOn();
+//  action 2: the N64 loads Cartridge data
+N64.loadCartridge();
+//  action 3: the N64 resets
+N64.reset();
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Drumpad
+//  action 1: the Drumpad triggers a sample
+Drumpad.triggerSample();
+//  action 2: the Drumpad sends MIDI data
+Drumpad.sendMIDI();
+//  action 3: the Drumpad applies effects to drum sounds
+Drumpad.applyFX();
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Roomba
+//  action 1: the Roomba vacuums
+Roomba.vacuum();
+//  action 2: the Roomba returns home
+Roomba.returnHome();
+//  action 3: the Roomba sends a status report
+Roomba.sendStatusReport();
 //  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Baby
+//  action 1: the Baby poops
+Baby.poop();
+//  action 2: the Baby fusses
+Baby.fuss();
+//  action 3: the Baby does something cute
+Baby.doSomethingCute();
 //  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Phone
+//  action 1: the Phone rings
+Phone.ring();
+//  action 2: the Phone plays back a voice mail
+Phone.playVoicemail();
+//  action 3: the phone sends a reminder
+Phone.sendReminder();
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
-
-
+//  Noun: Computer Display
+//  action 1: the Computer Display auto-detecs an input
+ComputerDisplay.autoDetectInput();
+//  action 2: the Computer Display changes resolution
+ComputerDisplay.changeResolution();
+//  action 3: the Computer Display auto sleeps
+ComputerDisplay.autoSleep();
 #endif
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
+ Commit your changes by clicking on the Source Control panel on the left,
+ entering a message, and click [Commit and push].
+
+ If you didn't already:
     Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
+    pin the pull request link and this repl.it link to our DM thread in a single
+ message.
 
  send me a DM to review your pull request when the project is ready for review.
 
@@ -150,8 +150,7 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
  */
 
 #include <iostream>
-int main()
-{
-    std::cout << "good to go" << std::endl;
-    return 0;
+int main() {
+  std::cout << "good to go" << std::endl;
+  return 0;
 }
